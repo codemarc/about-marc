9b42628cccd1389662f956f573d78348823eaff7
Marc J. Greenberg <marc@buildinglink.com>
2019-12-11 06:43:45 -0500

    datafile updates

---
+++ config.js
@@ -0,0 +1,95 @@
+export default [
+  {
+    section: 'Sites',
+    links: [
+      { label: 'Web', name: 'Live', href: 'http://www.buildinglink.com', name2: 'Public', href2: 'http://www.buildinglink.com/public' },
+      { label: 'Api..', name: 'Live', href: 'https://developer.buildinglink.com/', name2: 'Dev', href2: 'https://bl-api-mgmt-dev.azure-api.net' },
+      { name: 'Data Admin', href: 'https://blvuedevdma.z13.web.core.windows.net/' },
+      { label: 'vuesharedauth', name: '', href: '' },
+      { name: 'ms1: Login', href: 'https://vuesharedauth-dev.blkqa.com/v2/global/login/login.aspx' },
+      { name: 'ms1: Announce', href: 'https://vuesharedauth-dev.blkqa.com/v2/mgmt/announcements/viewscrollingannouncements.aspx' },
+      { name: 'ms1: Auth', href: 'https://auth-vuesharedauth-dev.blkqa.com/' },
+      { label: '', name: 'storage', href: 'http://bit.ly/34XUxzW', name2: 'wasa', href2: 'http://bit.ly/38aN8zy' },
+    ],
+  },
+  {
+    section: 'Communicate',
+    links: [
+      { name: 'Mail', href: 'http://mail.buildinglink.com' },
+      { name: 'ScreenCloud', href: 'https://signage.screen.cloud/screens' },
+      { name: 'ScreenConnect', href: 'http://connect.buildinglink.com' },
+      { name: 'Wufoo', href: 'https://buildinglink.wufoo.com/admin/' },
+      { name: 'Slack', href: 'https://buildinglink.slack.com/messages/general/' },
+      { name: 'Sharepoint', href: 'https://buildinglink.sharepoint.com/_layouts/15/sharepoint.aspx' },
+      { name: 'Team Site', href: 'http://bit.ly/2OVexhh' },
+    ],
+  },
+  {
+    section: 'Learn',
+    links: [
+      { name: 'Wiki', href: 'http://bit.ly/2YgGl2A' },
+      { name: 'old-doc', href: 'http://bldevs.helpdocsonline.com/pmip36q63s6ifqpnhmwya10tocxcwo2m' },
+      { name: 'Udemy', href: 'https://buildinglink.udemy.com/' },
+      { label: 'Git', name: 'hub', href: 'http://github.com', name2: 'ebook', href2: 'https://book.git-scm.com/book/en/v2' },
+      { label: `aware demo's`, name: '', href: '' },
+      { name: `solutions`, href: 'http://awareio.buildinglink.com/' },
+      {
+        label: `reports`,
+        name: 'blo',
+        href: 'https://awarestorageaccount.z20.web.core.windows.net/powerbiembed?pid=146',
+        name2: 'sol',
+        href2: 'https://awarestorageaccount.z20.web.core.windows.net/powerbiembed?pid=217',
+      },
+    ],
+  },
+  {
+    section: 'People',
+    links: [
+      { label: 'By', name: 'Dept', href: 'http://bit.ly/2LskwYR', name2: 'Employee', href2: 'http://bit.ly/2OVRXos' },
+      { name: 'CompanyWeb', href: 'http://companyweb/default.aspx' },
+      { name: 'Leadership', href: 'leadership.html' },
+      { name: 'BambooHR', href: 'https://buildinglink.bamboohr.com/' },
+    ],
+  },
+  {
+    section: 'TFS',
+    links: [
+      { name: 'Build, ', href: 'http://tfs.buildinglink.com/BuildingLink/Default/_build' },
+      { name: 'Queries', href: 'https://tfs.buildinglink.com/BuildingLink/Default/_queries' },
+      { name: 'Backlogs', href: 'https://tfs.buildinglink.com/BuildingLink/Default/_backlogs' },
+    ],
+  },
+  {
+    section: 'Source',
+    links: [
+      { name: 'Pull Requests', href: 'https://tfs.buildinglink.com/BuildingLink/Default/_git/BL-Website/pullrequests?_a=mine' },
+      { name: 'VB-Website', href: 'https://tfs.buildinglink.com/BuildingLink/Default/_versionControl' },
+      { name: 'BL-Website', href: 'https://tfs.buildinglink.com/BuildingLink/Default/_git/BL-Website' },
+      { name: 'VA-Website', href: 'https://tfs.buildinglink.com/BuildingLink/Default/_git/Vue-Architecture' },
+      { name: 'Data Admin', href: 'https://tfs.buildinglink.com/BuildingLink/Default/_git/Data-Models-Admin' },
+    ],
+  },
+  {
+    section: 'Cloud',
+    links: [
+      { name: 'Azure Portal', href: 'https://portal.azure.com/#@buildinglink.com/' },
+      { name: 'JSON Web Tokens', href: 'https://jwt.io/' },
+      { name: 'Amazon Console', href: 'http://console.aws.amazon.com/' },
+      { name: 'Amazon Dev', href: 'http://developer.amazon.com/' },
+    ],
+  },
+  {
+    section: 'Ops',
+    links: [
+      {
+        label: 'Sentry',
+        name: 'Web',
+        href: 'https://sentryweb.buildinglink.com/auth/login/buildinglink-web/',
+        name2: 'Apps',
+        href2: 'https://sentryapps.buildinglink.com/buildinglink-apps/',
+      },
+      { label: 'Rancher', name: '71', href: 'http://10.1.1.71', name2: '53', href2: 'https://rancher.buildinglink.com:8443' },
+      { name: 'RabbitMQ', href: ' http://amqp.buildinglink.local:15672' },
+    ],
+  },
+];
